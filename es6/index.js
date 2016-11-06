4017908393677b0deb5f82726b02ed0c3329afbe
Hassan Zamani <hsn.zamani@gmail.com>
2016-11-06 10:30:45 +0330

    Fixup

--- es6/index.js
+++ es6/index.js
@@ -1,7 +1,7 @@
-import Ex from './es6/Ex'
-import Map from './es6/Map'
-import List from './es6/List'
-import Enum from './es6/Enum'
+import Ex from './Ex'
+import Map from './Map'
+import List from './List'
+import Enum from './Enum'
 
 export { Ex, Map, List, Enum }
 export default Object.assign({}, Ex, { Map, List, Enum })
